f0b9b99a2ac8f8807b5372b0cfab0829dad6c2f8
mertsevimli <136606862+mertsevimli@users.noreply.github.com>
2024-05-04 11:09:21 +0300

    test

--- Konu07Donguler/Donguler.cs
+++ Konu07Donguler/Donguler.cs
@@ -7,13 +7,14 @@
 
             System.Console.WriteLine("Döngüler!");
             System.Console.WriteLine("1-For Döngüsü");
-            for (int i = 0; i < 10; i++)  //  döngü tanımlama 3 bloktan oluşuyor.
+            for (int i = 1; i <= 10; i++)  //  döngü tanımlama 3 bloktan oluşuyor.
             // int i = 0;  bildiğimiz değişken tanımlama. 
             //i < 10 ; i nin değeri 10 sayısından küçük olduğu sürece dönmeye devam et.
             // i++ : her döndüğünde i nin değerini 1 artır ki sonsuz döngü olsun
             {
                 System.Console.WriteLine("i nin değeri : " + i);
             }
+            System.Console.WriteLine();
         }
     }
 }
