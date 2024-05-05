35f69bf4a3d95eed46b34b39d8f71e039384d1c3
mertsevimli <136606862+mertsevimli@users.noreply.github.com>
2024-05-05 12:21:38 +0300

    string sinifi

--- Konu07Donguler/obj/Debug/net8.0/Konu07Donguler.AssemblyInfo.cs
+++ Konu07Donguler/obj/Debug/net8.0/Konu07Donguler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Konu07Donguler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f41ba0cbc73aaf43de42f7c42f1b2f47f7b783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ac2502ecc8ac1a752a9ccb2002aa5d03ac0eda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Konu07Donguler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Konu07Donguler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Konu10StringSinifi/StringSinifi.cs
@@ -0,0 +1,23 @@
+﻿namespace Konu10StringSinifi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("String Sınıfı!");
+            string degisken;
+            char karakter = 'z';
+            string metin = "String yan yana dizilmiş char yapısıdır.";
+        }
+
+
+static void Ornek()
+{
+    string birMetin = "Ankara Başkenttir.";
+    string birSayi = "123456789";
+System.String birTarih = "05.05.2024"; // string system kütüphanesinde bulunur.
+    string s = "Barış Manço";
+}
+
+    }
+}
